fc54a49de1c0a23ed7e0cbefe49e800364d8e869
479BetaCoder <ravikumar.pilla479@gmail.com>
2020-09-30 02:07:14 -0400

    modified basename

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from "./components/Footer/Footer";
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="https://479BetaCoder.github.io/ravi-pilla-portfolio">
       <div className="mainContainer">
         <Header></Header>
         <div>
