865f5057fc5d548ea860b66d8f2b56a3e5ab2c20
479BetaCoder <ravikumar.pilla479@gmail.com>
2020-09-30 01:52:12 -0400

    Updates

---
+++ precache-manifest.775e867b3cb5330b5f7ec6cdf5849a19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b851bc8c1db5f5b5ff2c226d8529ee19",
+    "url": "/ravi-pilla-portfolio/index.html"
+  },
+  {
+    "revision": "2715f15306021c3bfd24",
+    "url": "/ravi-pilla-portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "137523dab366729f1cf9",
+    "url": "/ravi-pilla-portfolio/static/js/2.0f447a1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravi-pilla-portfolio/static/js/2.0f447a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2715f15306021c3bfd24",
+    "url": "/ravi-pilla-portfolio/static/js/main.a3263c59.chunk.js"
+  },
+  {
+    "revision": "217148fe62c264f70d79",
+    "url": "/ravi-pilla-portfolio/static/js/runtime-main.547c202c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravi-pilla-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
